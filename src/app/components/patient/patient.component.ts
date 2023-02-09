cb2381defe52e6d90d8cfdd56f9dfc449142b38c
Sami <67370636+Sami-Fd@users.noreply.github.com>
2023-02-09 13:32:09 +0100

    Update patient.component.ts

--- src/app/components/patient/patient.component.ts
+++ src/app/components/patient/patient.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { AppointmentService } from "src/app/services/appointment/appointment.service";
 import { AuthService } from "src/app/services/auth/auth.service";
 import { PatientService } from "src/app/services/patient/patient.service";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: "app-patient",
@@ -17,7 +18,8 @@ export class PatientComponent {
 
   items: any;
   isLogged: boolean = false;
-
+  env: any = environment;
+  
   ngOnInit() {
     this.appointmentService.getAllAppointment().subscribe((data:any) => {
         console.log(data);
